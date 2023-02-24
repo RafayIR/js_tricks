9ea7b8ff642f303606ce07b5279cda647a2e09e4
Rafay Iqbal <43880610+RafayIR@users.noreply.github.com>
2023-02-24 15:52:09 +0500

    add new question s

--- interview.js
+++ interview.js
@@ -1,3 +1,4 @@
+// ARRAY MANIPULATION
 let array1 = [1,2]
 let array2 = [1,2]
 let counter = 0;
@@ -11,6 +12,7 @@ array1.map( (a1) => {
 
 console.log(counter)
 
+// ASSIGNMENT OPERATORS
 
 let a = 0
 let c = 0
@@ -18,3 +20,22 @@ c = a++ ? 10 : 100
 console.log('First',c)
 c = !a ? 10 : 100
 console.log('second' ,c)
+
+// CALCULATE LENGTH OF STRING WITHOUT USING LENGTH FUNCTION 
+
+function getStringLength(strvalue) {
+    let count = 0 
+    while(strvalue[count] !== undefined){
+        count++
+    }
+    return console.log(count)
+}
+let mystring = 'Helloworld'
+getStringLength(mystring)
+
+
+
+for (let i = 0 ; i < 5 ; i++)
+    for (let i = 0 ; i < 4 ; i++ )
+        console.log('Hello' )
+        console.log('world')
